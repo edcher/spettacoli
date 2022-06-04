6a5004e0256dc3f4f0d4047d6391ab00a08d26c2
edcher <rinachernyshova@mail.ru>
2022-06-05 00:36:45 +0200

    2 files modified

--- src/app/teatro/teatro.component.ts
+++ src/app/teatro/teatro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { TeatroService } from './teatro.service';
 
 export class postiTeatro {
@@ -11,13 +11,32 @@ export class postiTeatro {
   templateUrl: './teatro.component.html',
   styleUrls: ['./teatro.component.css']
 })
-export class TeatroComponent implements OnInit {
+export class TeatroComponent {
+  @Output() newTeatroEvent = new EventEmitter<string>();
   posti: Array<postiTeatro>;
   key: string;
 
   constructor(private service: TeatroService) { }
 
-  ngOnInit() {
+  creaTeatro(){
+    this.service.newPrenotazione().subscribe(
+      {
+        next: (x: any) => {
+          const key = x;
+          const platea = new Array(7).fill("").map(() => new Array(10).fill("x"));
+          const palco = new Array(4).fill("").map(() => new Array(6).fill("x"));
+          const prenotazione = platea.concat(palco);
+          this.service.setPrenotazione(key, prenotazione).subscribe(
+            {
+              next: (x: any) => {
+                this.newTeatroEvent.emit(key);
+              },
+              error: err => console.error(`Observer got an error: ${JSON.stringify(err)}`)
+            }
+          )
+        },
+        error: err => console.error(`Observer got an error: ${JSON.stringify(err)}`)
+      }
+    )
   }
-
 }
